f9667374a26da49d44014b43aea6e55929bef111
Jacob Washburn <jacobrwashburn@gmail.com>
2020-03-11 18:57:11 -0600

    get grades based on codeclimates get ref points endpoint

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import Projects from './Projects';
@@ -10,6 +10,30 @@ const Button = styled.button``;
 
 const LandingPage = ({state, setState, history}) => {
   const [token, setToken] = useState('');
+  const [grades, setGrades] = useState([]);
+  const [snapshots, setSnapshots] = useState([]);
+  console.log({grades});
+  console.log({snapshots});
+  useEffect(() => {
+    axios.get('https://api.codeclimate.com/v1/repos/5dcc150b234ac30e83009498/ref_points?filter[branch]=master', {headers: {Authorization:'Token token=a8bd69e0e7cafd98a0581184ae71ffbf13b53cd8'}})
+      .then(res => {
+        console.log('res', res);
+        const snapshotList = res.data.data.map(stuff => stuff.relationships.snapshot.data.id);
+        setSnapshots(snapshotList)
+      })
+  },[]);
+
+  useEffect(() => {
+    Promise.all(snapshots.map(snapshot => axios.get(`https://api.codeclimate.com/v1/repos/5dcc150b234ac30e83009498/snapshots/${snapshot}`, {headers: {Authorization:'Token token=a8bd69e0e7cafd98a0581184ae71ffbf13b53cd8'}})
+      .then(res => res.data.data.attributes.ratings[0].letter)
+      .catch(error => console.log('error res', error))
+    ))
+      .then(res => {
+        setGrades(res)
+      })
+
+  },[snapshots]);
+
   const handleChange = e => {
     setToken(e.target.value);
   };
@@ -30,23 +54,25 @@ const LandingPage = ({state, setState, history}) => {
     });
     history.push('/chooseorg');
   };
-
+{/*<Div>*/}
+{/*        <Form onSubmit={handleSubmit}>*/}
+{/*          <Input*/}
+{/*            required*/}
+{/*            type='text'*/}
+{/*            name='token'*/}
+{/*            value={token}*/}
+{/*            placeholder='Token'*/}
+{/*            onChange={handleChange}*/}
+{/*          />*/}
+{/*          <Button type='submit'>Submit</Button>*/}
+{/*        </Form>*/}
+{/*      </Div>*/}
   if ( !state.orgs.length) {
     return (
-      <Div>
-        <Form onSubmit={handleSubmit}>
-          <Input
-            required
-            type='text'
-            name='token'
-            value={token}
-            placeholder='Token'
-            onChange={handleChange}
-          />
-          <Button type='submit'>Submit</Button>
-        </Form>
-      </Div>
 
+    <div>
+      {grades.map(grade => grade ? <p>{grade}</p>:<p>No grade.</p>)}
+    </div>
     );
   } else {
     return (
